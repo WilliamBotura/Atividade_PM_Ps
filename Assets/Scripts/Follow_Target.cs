7fd7f4dcf4c740829a429c614c0b8615761b9fc3
William Botura <williamdigitalwork@gmail.com>
2019-06-03 23:22:17 -0300

    v2

--- Assets/Scripts/Follow_Target.cs
+++ Assets/Scripts/Follow_Target.cs
@@ -1,18 +1,67 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Follow_Target : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform Target;
+    public string TargetTag;
+    public float velMovimento = 3.0f;
+    public bool lookAt = false;
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ProcurarTarget();
+        Movimentar();
+        Rotacionar();
+    }
+
+    private void ProcurarTarget()
+    {
+        if (TargetTag == "" || Target != null)
+        {
+            return;
+        }
+
+        GameObject[] Targets = GameObject.FindGameObjectsWithTag(TargetTag);
+        Transform possivelTarget = null;
+
+        foreach (GameObject checarTarget in Targets)
+        {
+            if (possivelTarget == null)
+            {
+                possivelTarget = checarTarget.transform;
+            }
+        }
+        if (possivelTarget != null)
+        {
+            Target = possivelTarget.transform;
+        }
+    }
+
+    private void Movimentar()
+    {
+        if (lookAt || Target == null)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * velMovimento);
+        }
+        else if (Target != null)
+        {
+            Vector3 dir = (Target.position - transform.position).normalized;
+            transform.Translate(dir * velMovimento * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void Rotacionar()
+    {
+        if (lookAt || Target != null)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Target.position - transform.position), Time.deltaTime * velMovimento);
+        }
     }
 }
--- Assets/Scripts/Heli_Script.cs
+++ Assets/Scripts/Heli_Script.cs
@@ -19,12 +19,6 @@ public class Heli_Script : MonoBehaviour
 
     private void Movimentar()
     {
-        //float aleatorio = UnityEngine.Random.Range(-180, 180);
-        //if (aleatorio == -180 || aleatorio == -90 || aleatorio == 90 || aleatorio == 180)
-        //{
-        //    Vector3 rotacao = new Vector3(0, aleatorio, 0);
-        //    Quaternion rotation = Quaternion.Euler(rotacao);
-        //}
         transform.Translate(Vector3.forward * Time.deltaTime);
     }
 }
